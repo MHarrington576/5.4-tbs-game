5257dfc6fc3b035b347e28eac2771522569f8d98
Matthew Harrington <mattharrington576@gmail.com>
2016-10-14 11:26:05 -0400

    Character selection begins

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,11 +1,31 @@
 var $ = require('jquery');
-var _ = require('underscore');
-var models = require('models.js');
-var listTemplate = require('../templates/listTemplate.hbs');
+var models = require('./models');
+var listTemplate = require('../templates/listTemplate.hbs')
+
+var Sora;
+var Roxas;
+var Riku;
+var Kairi;
+
+var player = undefined;
+>>>>>>> Character selection begins
 
 function launchGame(){
   //display message
 ()}
+
+$('.character-select-photo').on('click', function(event){
+  if(this.id == '#select-sora') {
+    player = 'Sora';
+  } if else(this.id == '#select-roxas') {
+    player = 'Roxas';
+  } if else(this.id == '#select-riku') {
+    player = 'Riku';
+  } if else(this.id == '#select-kairi') {
+    player = 'Kairi';
+  }
+  return player;
+});
 
 $('.attack-btn').on('click', function(event){
   event.preventDefault();
